b84bfee27ebbbd204f966253d0783dd65393c40c
GracePimentel23 <gp3326@rit.edu>
2024-09-25 18:07:12 -0400

    made changed working on 2a

--- src/test/java/org/gradle/main/MainTestProj.java
+++ src/test/java/org/gradle/main/MainTestProj.java
@@ -2,7 +2,5 @@ package org.gradle.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, This is for Makefile.prog - main!");
-    }
-}
-
+        System.out.println("Hello, This is Makefile.proj - test file!");
+       } 
